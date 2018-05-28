7250f496787f3c6a79653f790155eace678959c2
Oleg Sergeev <farodit@gmail.com>
2018-05-29 01:36:03 +0300

    First try

---
+++ Modules/Questions.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace bot.Modules
+{
+    public class Questions : ModuleBase<SocketCommandContext>
+    {
+        [Command("Test")]
+        public async Task testfunc()
+        {
+            await Context.Channel.SendMessageAsync("Работает");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace bot
+{
+    class Program
+    {
+		private DiscordSocketClient client;
+
+        private MessageHandler handler;
+
+        static void Main(string[] args)
+        {
+			new Program().StartAsync().GetAwaiter().GetResult();
+        }
+
+		private async Task StartAsync()
+		{
+			client = new DiscordSocketClient();
+
+            
+			await client.LoginAsync(TokenType.Bot, "NDUwNjUzNTMzNDk1MDk5Mzkz.De2cPA.26XcebFedCvxlXmczqAggo9_iFQ");
+			await client.StartAsync();
+
+            handler = new MessageHandler(client);
+
+            await Task.Delay(-1);
+
+		}
+    }
+}
